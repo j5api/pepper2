ec380e085c497ef6a1782efc7cabd9af8a4f05f0
Dan Trickey <dan@trickey.io>
2019-11-12 19:01:12 +0000

    Get drive mount path

--- pepper2/daemon/daemon.py
+++ pepper2/daemon/daemon.py
@@ -12,22 +12,8 @@ from pepper2.daemon.controller import Controller
 LOGGER = logging.getLogger(__name__)
 
 loop = GLib.MainLoop()
-
-
-def disk_signal(path, data):
-    LOGGER.debug(f"Received event from {path}")
-    if "/org/freedesktop/UDisks2/jobs/" in path:
-        for job in data.keys():
-            event_data = data[job]
-            if "Operation" in event_data.keys():
-                if event_data["Operation"] == "filesystem-mount":
-                    LOGGER.info(f"Mount Event detected at {path}")
-                    if 'Objects' in event_data.keys() and len(event_data["Objects"]) > 0:
-                        print(event_data["Objects"])
-                    else:
-                        LOGGER.warning("No information on drive available. Aborting.")
-                if event_data["Operation"] == "cleanup":
-                    LOGGER.info(f"Removal Event detected at {path}")
+# We must use the system bus, as that is where udisks is
+bus = SystemBus()
 
 
 def main() -> None:
@@ -36,11 +22,7 @@ def main() -> None:
 
     LOGGER.info(f"Starting v{__version__}.")
 
-    # We must use the system bus, as that is where udisks is
-    bus = SystemBus()
-
-    pepperd = PepperDaemon(bus)
-    pepperd.start()
+    pepperd = PepperDaemon()
 
     try:
         loop.run()
@@ -50,14 +32,10 @@ def main() -> None:
 
 class PepperDaemon:
 
-    def __init__(self, bus: Bus):
-        self.bus = Bus
-
-    def start(self):
+    def __init__(self):
         try:
             # Publish our controller on the bus.
-            self.bus.publish("uk.org.j5.pepper2", Controller(loop))
-
+            bus.publish("uk.org.j5.pepper2", Controller(loop))
         except RuntimeError as e:
             if str(e) == "name already exists on the bus":
                 LOGGER.error("pepperd is already running.")
@@ -67,9 +45,9 @@ class PepperDaemon:
                 raise
 
         # Subscribe to udisks
-        udisks = self.bus.get(".UDisks2")
+        self.udisks = bus.get(".UDisks2")
 
-        self.disk_signal_handler = udisks.InterfacesAdded.connect(disk_signal)
+        self.disk_signal_handler = self.udisks.InterfacesAdded.connect(self.disk_signal)
 
         notify("READY=1")
         LOGGER.info(f"Ready.")
@@ -79,6 +57,33 @@ class PepperDaemon:
         LOGGER.info("Stopping.")
         self.disk_signal_handler.disconnect()
         loop.quit()
+
+    def disk_signal(self, path, data):
+        LOGGER.debug(f"Received event from {path}")
+        if "/org/freedesktop/UDisks2/jobs/" in path:
+            for job in data.keys():
+                event_data = data[job]
+                if "Operation" in event_data.keys():
+                    if event_data["Operation"] == "filesystem-mount":
+                        LOGGER.info(f"Mount Event detected at {path}")
+                        if 'Objects' in event_data.keys() and len(event_data["Objects"]) > 0:
+                            disk_bus_path = event_data["Objects"][0]
+                            block_device = bus.get(".UDisks2", disk_bus_path)
+                            mount_points = block_device.MountPoints
+                            if len(mount_points) != 0:
+                                mount_point = mount_points[0]
+                                chars = map(chr, mount_point)
+                                mount_point_str = "".join(chars)
+                                LOGGER.info(f"Drive mounted: {mount_point_str}")
+                            else:
+                                LOGGER.warning(f"No mountpoints available for {disk_bus_path}")
+                        else:
+                            LOGGER.warning("No information on drive available. Aborting.")
+                    if event_data["Operation"] == "cleanup":
+                        LOGGER.info(f"Removal Event detected at {path}")
+
+
+
 
 
 if __name__ == "__main__":
