ac639e3735a1d7582ee6740bd8d0810e650bf57a
Dan Trickey <dan@trickey.io>
2019-11-20 16:19:53 +0000

    We should support python 3.6

--- pepper2/usbinfo.py
+++ pepper2/usbinfo.py
@@ -1,14 +1,20 @@
 """USBInfo class."""
 
-from dataclasses import dataclass
 from pathlib import Path
 
 
-@dataclass
 class USBInfo:
     """Represents Information about an inserted USB."""
 
-    dbus_job_path: str
-    dbus_fs_path: str
-    dbus_drive_path: str
-    mount_path: Path
+    def __init__(
+            self,
+            *,
+            dbus_job_path: str,
+            dbus_fs_path: str,
+            dbus_drive_path: str,
+            mount_path: Path,
+    ):
+        self.dbus_job_path = dbus_job_path
+        self.dbus_fs_path = dbus_fs_path
+        self.dbus_drive_path = dbus_drive_path
+        self.mount_path = mount_path
