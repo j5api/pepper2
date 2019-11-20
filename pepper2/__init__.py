9f3270c19b7eec4933f92ee13c253a13c8627c6e
Dan Trickey <dan@trickey.io>
2019-11-20 16:34:52 +0000

    Use Pepper2Exception in cli

--- pepper2/__init__.py
+++ pepper2/__init__.py
@@ -5,11 +5,12 @@ Library to interaction with robot management daemon.
 """
 
 from .api import Pepper2
+from .error import Pepper2Exception
 
 __all__ = [
     "__version__",
     "Pepper2",
-
+    "Pepper2Exception",
 ]
 
 __version__ = "0.1.0"
--- pepper2/cli/status.py
+++ pepper2/cli/status.py
@@ -2,13 +2,17 @@
 
 import click
 
-from pepper2 import Pepper2
+from pepper2 import Pepper2, Pepper2Exception
 
 
 @click.command()
 def status() -> None:
     """Get the status of pepper2."""
-    pepper2 = Pepper2()
+    try:
+        pepper2 = Pepper2()
+    except Pepper2Exception as e:
+        click.secho(str(e), err=True, fg="red")
+        exit(1)
 
     print(f"Pepper2 - Robot Management Daemon v{pepper2.daemon_version}")
     print(f"\tStatus: {pepper2.daemon_status.name}")
