e8e6de8121f6a1e74ea2f3040c890f8511c9c71f
Dan Trickey <dan@trickey.io>
2019-12-22 20:33:42 +0000

    Separate controller xml into a separate file.
    Fixes #8

--- pepper2/daemon/controller.py
+++ pepper2/daemon/controller.py
@@ -4,6 +4,7 @@ from threading import Lock
 from typing import List, Tuple
 
 from gi.repository import GLib
+from pkg_resources import resource_string
 
 from pepper2 import __version__
 from pepper2.drives import DriveGroup
@@ -12,29 +13,10 @@ from pepper2.status import DaemonStatus
 LOGGER = logging.getLogger(__name__)
 
 
-class Controller:  # noqa: D400 D205 D208
-    """
-        <!-- Daemon Controller -->
-        <!-- All state should be stored on this class. -->
-        <!-- This XML is read by pydbus. -->
-        <node>
-            <interface name='uk.org.j5.pepper2.Controller'>
-                <method name='get_version'>
-                    <arg type='s' name='version' direction='out'/>
-                </method>
-                <method name='get_status'>
-                    <arg type='s' name='status' direction='out'/>
-                </method>
-                <method name='get_drive_list'>
-                    <arg type='as' name='drives' direction='out'/>
-                </method>
-                <method name='get_drive'>
-                    <arg type='s' name='uuid' direction='in' />
-                    <arg type='(bssi)' name='drive' direction='out' />
-                </method>
-            </interface>
-        </node>
-    """
+class Controller:
+    """Pepper2 DBUS controller."""
+
+    dbus = resource_string(__name__, "controller.xml").decode('utf-8')
 
     def __init__(self, loop: GLib.MainLoop):
         self._status = DaemonStatus.STARTING
