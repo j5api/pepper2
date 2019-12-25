77ab642374421fa56c2194439e41cf1cd2f3b197
Dan Trickey <dan@trickey.io>
2019-12-25 21:02:32 +0000

    Add abstract usercode driver

---
+++ pepper2/usercode_driver/__init__.py
@@ -0,0 +1,1 @@
+"""Usercode Drivers."""
---
+++ pepper2/usercode_driver/usercode_driver.py
@@ -0,0 +1,26 @@
+"""Base class for User Code Drivers."""
+
+from abc import ABCMeta, abstractmethod
+
+
+class UserCodeDriver(metaclass=ABCMeta):
+    """
+    User Code Driver class.
+
+    This class defines a set of functionality that must be
+    implemented in a usercode driver.
+
+    A usercode driver runs and manages some user code through a
+    uniform interface. This allows us to execute usercode in a
+    variety of formats and environments, depending on the kit.
+    """
+
+    @abstractmethod
+    def start_execution(self) -> None:
+        """Start the execution of the code."""
+        raise NotImplementedError  # pragma: nocover
+
+    @abstractmethod
+    def stop_execution(self) -> None:
+        """Stop the execution of the code."""
+        raise NotImplementedError  # pragma: nocover
