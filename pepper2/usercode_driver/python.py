8421729f487a5e79940671576995b99d4f8ca291
Dan Trickey <dan@trickey.io>
2019-12-26 21:42:50 +0000

    Use SIGCHLD to handle premature termination of usercode process

--- pepper2/usercode_driver/python.py
+++ pepper2/usercode_driver/python.py
@@ -1,13 +1,19 @@
 """Python Usercode Driver."""
 
 import logging
-from subprocess import Popen, DEVNULL, TimeoutExpired
-from signal import SIGTERM, SIGKILL, SIGCHLD
-from typing import Optional
+from signal import SIGCHLD, SIGKILL, SIGTERM, Signals, getsignal, signal
+from subprocess import DEVNULL, Popen, TimeoutExpired
+from types import FrameType
+from typing import TYPE_CHECKING, Optional
 
 from pepper2.drives import Drive
 
 from .usercode_driver import UserCodeDriver
+
+if TYPE_CHECKING:
+    # _HANDLER is only available in typeshed.
+    from signal import _HANDLER
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,29 +25,56 @@ class PythonDriver(UserCodeDriver):
     TODO: Name this class something better.
     """
 
-    process: Optional[Popen]
+    _process: Optional[Popen]
 
     def __init__(self, drive: Drive):
         self.drive = drive
-        self.process = None
+        self._process = None
+        self._original_sigchld: _HANDLER = getsignal(SIGCHLD)
 
     def start_execution(self) -> None:
         """Start the execution of the code."""
-        self.process = Popen(
+        signal(SIGCHLD, self.sigchld_handler)
+        self._process = Popen(
             ["python3", "main.py"],
             stdin=DEVNULL,
             cwd=self.drive.mount_path,
             start_new_session=True,  # Put the process in a new process group
         )
 
-        LOGGER.info(f"Usercode process started with pid {self.process.pid}")
+        LOGGER.info(f"Usercode process started with pid {self._process.pid}")
 
     def stop_execution(self) -> None:
         """Stop the execution of the code."""
-        LOGGER.info(f"Sent SIGTERM to pid {self.process.pid}")
-        self.process.send_signal(SIGTERM)
-        try:
-            self.process.communicate(timeout=5)
-        except TimeoutExpired:
-            pass
-        self.process.send_signal(SIGKILL)
+        self._reset_sigchld()
+
+        if self._process is not None:
+            LOGGER.info(f"Sent SIGTERM to pid {self._process.pid}")
+            self._process.send_signal(SIGTERM)
+            try:
+                self._process.communicate(timeout=5)
+            except TimeoutExpired:
+                pass
+            self._process.send_signal(SIGKILL)
+            self._process = None
+        else:
+            LOGGER.info("No usercode process to stop.")
+
+    def sigchld_handler(self, _: Signals, __: FrameType) -> None:
+        """
+        Handler for SIGCHLD.
+
+        This is called when the child process of this process dies.
+        """
+        self._reset_sigchld()
+        return_code = self._process.poll()
+
+        if return_code == 0:
+            LOGGER.info("Usercode finished successfully.")
+        else:
+            LOGGER.info(f"Usercode finished unsuccessfully (return code: {return_code}).")
+        self._process = None
+
+    def _reset_sigchld(self) -> None:
+        """Reset the SIGCHLD handler to the original."""
+        signal(SIGCHLD, self._original_sigchld)
