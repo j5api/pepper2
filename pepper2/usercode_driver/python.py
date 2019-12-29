ed2e4d7f0eab0b1973f9891b1c0c2b9ed33d34b1
Dan Trickey <dan@trickey.io>
2019-12-29 18:06:29 +0000

    Add the Logger Thread, not actually logging though
    This thread is an alternative to external programs that we used in the
    past, such as `script`. This is a pure-python implementation of this
    such that we can get exactly the functionality that we need.
    
    Due to the nature of logging a separate process, it makes a lot of sense
    to have this running in a daemon / background thread. However, to make
    this easier to program, the Logger Thread will not edit any data in the
    main program thread, and will be the only thing that holds references to
    it's own resources (e.g the log file handle).
    
    The only external interaction that there should be beyond the starting
    og the thread, is notifying it via `.stop()` that the process that it is
    monitoring has stopped, although the logger thread may have noticed this
    already.

--- pepper2/usercode_driver/python.py
+++ pepper2/usercode_driver/python.py
@@ -3,6 +3,8 @@
 import logging
 from signal import SIGCHLD, SIGKILL, SIGTERM, Signals, getsignal, signal
 from subprocess import DEVNULL, Popen, TimeoutExpired
+from threading import Thread
+from time import sleep
 from types import FrameType
 from typing import TYPE_CHECKING, Optional
 
@@ -18,6 +20,46 @@ if TYPE_CHECKING:
 LOGGER = logging.getLogger(__name__)
 
 
+class LoggerThread(Thread):
+    """
+    A thread that will log the output of the process.
+
+    This thread does not modify any data in the program, it simply
+    logs the output of the child process to the appropriate
+    destinations. This is used as a more flexible, pure-python
+    alternative to solutions such as ``script``.
+    """
+
+    def __init__(self, process: Popen, drive: Drive):
+        super().__init__(daemon=True)
+        self._process = process
+        self._log_file = open(
+            drive.mount_path.joinpath("log.txt"),
+            "w",
+        )
+        self.log = True
+
+    def run(self) -> None:
+        """Log the process."""
+        LOGGER.info("Logger Thread Started.")
+        self._log_line_to_file("=== LOG STARTED ===")
+        while self.log:
+            print("logging")
+            sleep(0.5)
+        self._log_line_to_file("=== LOG FINISHED ===")
+        self._log_file.close()
+        LOGGER.info("Logger Thread Exiting.")
+
+    def stop(self) -> None:
+        """Stop logging."""
+        self.log = False
+
+    def _log_line_to_file(self, line: str) -> None:
+        """Log a line to the logfile."""
+        self._log_file.write(f"{line}\n")
+        self._log_file.flush()
+
+
 class PythonDriver(UserCodeDriver):
     """
     Usercode driver to execute Python.
@@ -26,10 +68,14 @@ class PythonDriver(UserCodeDriver):
     """
 
     _process: Optional[Popen]
+    _logger: Optional[LoggerThread]
 
     def __init__(self, drive: Drive):
         self.drive = drive
+
         self._process = None
+        self._logger = None
+
         self._original_sigchld: _HANDLER = getsignal(SIGCHLD)
 
     def start_execution(self) -> None:
@@ -41,6 +87,8 @@ class PythonDriver(UserCodeDriver):
             cwd=self.drive.mount_path,
             start_new_session=True,  # Put the process in a new process group
         )
+        self._logger = LoggerThread(self._process, self.drive)
+        self._logger.start()
 
         LOGGER.info(f"Usercode process started with pid {self._process.pid}")
 
@@ -55,8 +103,9 @@ class PythonDriver(UserCodeDriver):
                 self._process.communicate(timeout=5)
             except TimeoutExpired:
                 pass
+            LOGGER.info(f"Sent SIGKILL to pid {self._process.pid}")
             self._process.send_signal(SIGKILL)
-            self._process = None
+            self._cleanup()
         else:
             LOGGER.info("No usercode process to stop.")
 
@@ -77,8 +126,15 @@ class PythonDriver(UserCodeDriver):
                 LOGGER.info(
                     f"Usercode finished unsuccessfully (return code: {return_code}).",
                 )
-            self._process = None
+            self._cleanup()
 
     def _reset_sigchld(self) -> None:
         """Reset the SIGCHLD handler to the original."""
         signal(SIGCHLD, self._original_sigchld)
+
+    def _cleanup(self) -> None:
+        """Clean up from a running process."""
+        self._process = None
+
+        if self._logger is not None:
+            self._logger.stop()
