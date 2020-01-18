f9508e94604b743f78092a9d49a53688a93159c5
Dan Trickey <dan@trickey.io>
2020-01-18 18:32:29 +0000

    Add exception handling to PublishableGroup

--- pepper2/daemon/publishable_group.py
+++ pepper2/daemon/publishable_group.py
@@ -2,6 +2,7 @@
 
 from typing import Any, Dict, Iterator, MutableMapping, NamedTuple, TypeVar
 
+from gi.repository import GLib
 from pydbus.auto_names import auto_bus_name, auto_object_path
 from pydbus.bus import Bus
 from pydbus.registration import ObjectRegistration
@@ -41,20 +42,29 @@ class PublishableGroup(MutableMapping[str, U]):
         self._dict: Dict[str, PublishedObject] = {}  # type: ignore
 
     def __setitem__(self, k: str, v: U) -> None:
+        if k in self._dict.keys():
+            raise KeyError("Objects in PublishableGroup cannot be overridden.")
         bus_path = auto_object_path(self._bus_path, k).replace('-', '_')
-        registration = self._bus.register_object(bus_path, v, None)
 
-        published_object = PublishedObject(
-            bus_path=bus_path,
-            registration=registration,
-            object=v,
-        )
+        try:
+            registration = self._bus.register_object(bus_path, v, None)
 
-        self._dict[k] = published_object
+            published_object = PublishedObject(
+                bus_path=bus_path,
+                registration=registration,
+                object=v,
+            )
+
+            self._dict[k] = published_object
+        except GLib.Error as e:
+            raise ValueError("Unable to publish the object on DBus.") from e
 
     def __delitem__(self, k: str) -> None:
         published_object = self._dict.pop(k)
-        published_object.registration.unregister()
+        try:
+            published_object.registration.unregister()
+        except GLib.Error as e:
+            raise ValueError("Unable to remove the object from DBus.") from e
 
     def __getitem__(self, k: str) -> U:
         return self._dict[k].object  # type: ignore
