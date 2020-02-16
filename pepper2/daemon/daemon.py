17f07f9a7fe27fc930b6dc994ce78fb92b0c10c5
Dan Trickey <dan@trickey.io>
2020-02-16 21:17:26 +0000

    Remove functionality from PepperDaemon class.

--- pepper2/daemon/daemon.py
+++ pepper2/daemon/daemon.py
@@ -1,7 +1,5 @@
 """Pepperd App."""
 import logging
-from signal import SIGHUP, SIGINT, SIGTERM, Signals, signal
-from types import FrameType
 
 import click
 
@@ -33,19 +31,6 @@ class PepperDaemon:
 
     def __init__(self) -> None:
         LOGGER.info(f"Starting v{__version__}.")
-
-        # Shutdown gracefully
-        signal(SIGHUP, self._signal_stop)
-        signal(SIGINT, self._signal_stop)
-        signal(SIGTERM, self._signal_stop)
-
-    def stop(self) -> None:
-        """Stop the daemon."""
-        pass
-
-    def _signal_stop(self, signal: Signals, __: FrameType) -> None:
-        LOGGER.debug(f"Received {Signals(signal).name}")
-        self.stop()
 
 
 if __name__ == "__main__":
